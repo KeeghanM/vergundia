a1a7b7bf371931408d5ba79f83de42eb6d715335
Keeghan McGarry <keeghan_m@live.com>
2025-01-14 07:13:35 +0000

    Initial Setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import partytown from '@astrojs/partytown'
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    tailwind(),
+    partytown({
+      config: {
+        debug: true,
+      },
+    }),
+  ],
+})
